c084f53dc40a5cb1c6cf359c7ca40fd49682ee1b
HandyWote <18666119673@163.com>
2025-09-14 17:19:58 +0800

    25.9.14

--- test.py
+++
@@ -1,8 +0,0 @@
-def power_set_iterative(s):
-    result = [[]]
-    for elem in s:
-        new_subsets = [subset + [elem] for subset in result]
-        result += new_subsets
-    return result
-
-print(power_set_iterative([1,2]))

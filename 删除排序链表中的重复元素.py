883bafa9669cb81a4a03de13f95c27ba6858e020
HandyWote <18666119673@163.com>
2025-10-19 22:00:42 +0800

    25.10.19

---
+++ 删除排序链表中的重复元素.py
@@ -0,0 +1,11 @@
+class Solution:
+    def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if head is None:
+            return None
+        cur = head
+        while cur.next:
+            if cur.next.val == cur.val:
+                cur.next = cur.next.next
+            else:
+                cur = cur.next
+        return head
